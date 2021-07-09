50679302a720f45d2d5ccef14517f4abd4036d9d
iliand1 <82459673+iliand1@users.noreply.github.com>
2021-07-09 17:46:04 +0300

    Add files via upload

--- get rid of excess spaces.py
+++ get rid of excess spaces.py
@@ -1,8 +1,8 @@
-raw_text = open("lox1.txt", "r") #Opens raw file
+raw_text = open("input.txt", "r") #Opens raw file
 data = raw_text.read()
 raw_text.close()
 clean_text = data.split()#Transforms into dict
 c = ' '.join(clean_text)#Transforms into string
-b = open("otvet1.txt", "w")#Write result to new file
+b = open("outpuy.txt", "w")#Write result to new file
 b.write(c)
 b.close()
