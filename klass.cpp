fe6c3c30c061ad1e481f18b1bb75cf0bfbccbbbd
iliand <82459673+iliand1@users.noreply.github.com>
2021-07-18 21:31:39 +0300

    вф

--- klass.cpp
+++ klass.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+/*
+https://ru.stackoverflow.com/questions/642331/%D0%9A%D0%B0%D0%BA-%D1%81%D1%87%D0%B8%D1%82%D1%8B%D0%B2%D0%B0%D1%82%D1%8C-%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D1%83%D1%80%D1%83-%D0%B8%D0%B7-%D1%84%D0%B0%D0%B9%D0%BB%D0%B0
+*/
 struct klass
 {
     char name[20];
